62bf5746beda387fbb369f9a6e47a81b8fb208da
hsharrison <henry.schafer.harrison@gmail.com>
2015-01-04 01:04:22 -0500

    mogrify

--- nfldbproj/names.py
+++ nfldbproj/names.py
@@ -1,7 +1,7 @@
 """Handling for names that can't be matched."""
 from __future__ import absolute_import, division, print_function
 
-from nfldb import Tx
+from nfldb import Tx, player_search
 from nfldb.update import log
 
 from nfldbproj.update import lock_tables
@@ -16,10 +16,6 @@ def add_name_disambiguations(db, ids_by_names):
     log('Writing rows to name_disambiguation...')
     with Tx(db) as c:
         lock_tables(c, ['name_disambiguation'])
-        c.execute('''
-            INSERT INTO name_disambiguation (name_as_scraped, player_id) VALUES
-                {}
-        '''.format(
-            ', '.join("('{}', '{}')".format(*item) for item in ids_by_names.items())
-        ))
+        c.execute('INSERT INTO name_disambiguation (name_as_scraped, player_id) VALUES '
+                  + ', '.join(c.mogrify('(%s, %s)', item) for item in ids_by_names.items()))
     log('done.')
