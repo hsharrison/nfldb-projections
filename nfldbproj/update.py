b0c9aeae874caae60f7fc7984c8c9e44b3501d9b
hsharrison <henry.schafer.harrison@gmail.com>
2015-01-07 15:27:04 -0500

    docstrings for "private" functions

--- nfldbproj/update.py
+++ nfldbproj/update.py
@@ -64,11 +64,21 @@ def insert_metadata(db, metadata):
 
 
 def _extract_and_insert(cursor, table, data):
+    """
+    Insert row into a metadata table `table` (only if it doesn't already exist)
+    using only those elements of dictionary `data` that correspond to columns in `table`.
+
+    """
     if all(pk in data for pk in METADATA_PRIMARY_KEYS[table]):
         _insert_if_new(cursor, table, _subdict(data, _columns(cursor, table)))
 
 
 def _insert_if_new(cursor, table, data):
+    """
+    Check if row specified in dictionary `data` exists in table `table`,
+    and if it doesn't, insert it.
+
+    """
     pk_only_data = _subdict(data, METADATA_PRIMARY_KEYS[table])
     if not _exists(cursor, table, pk_only_data):
         log('inserting new {}...'.format(table), end='')
@@ -77,15 +87,18 @@ def _insert_if_new(cursor, table, data):
 
 
 def _insert_dict(cursor, table, data):
+    """Insert row into `table` as specified by dictionary `data`."""
     cursor.execute('INSERT INTO {} ({}) VALUES ({})'.format(table, *_query_fields(data)), data)
 
 
 def _exists(cursor, table, data):
+    """Check if the row specified by dictionary `data` exists in table `table`."""
     cursor.execute('SELECT 1 FROM {} WHERE ({}) = ({})'.format(table, *_query_fields(data)), data)
     return bool(cursor.fetchone())
 
 
 def _columns(cursor, table):
+    """Return the columns of a table as a list."""
     cursor.execute('''
         SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = %s
     ''', (table, ))
@@ -93,10 +106,26 @@ def _columns(cursor, table):
 
 
 def _tables_from_headers(headers):
+    """
+    Given an iterable of column names, return the set of table names that it refers to.
+    Works with "data" tables, not "metadata" tables.
+
+    """
     return {table for field, table in _DATA_TABLES_BY_UNIQUE_FIELD.items() if field in headers}
 
 
 def _query_fields(data):
+    """
+    Generate the strings needed in a `cursor.execute` call to insert a dictionary.
+
+    Example:
+        >>> cols, values = _query_fields({'a': 1, 'b': 2})
+        >>> cols
+        'a, b'
+        >>> values
+        '%(a)s, %(b)s'
+
+    """
     keys = data.keys()  # Do this once to avoid any issue with dictionary order.
     column_fields = ', '.join(keys)
     value_fields = ', '.join('%({})s'.format(field) for field in data.keys())
